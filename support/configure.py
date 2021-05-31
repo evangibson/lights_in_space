366563398becc9021c9210fecd0b2c8b59275538
evangibson <evangibson2794@gmail.com>
2021-05-31 19:58:57 -0400

    stood up helper class

--- support/configure.py
+++ support/configure.py
@@ -2,10 +2,35 @@
 
 import os
 import sys
+import json
+
+# Static variables (can be replaced by standing data dictionaries)
+
+# List of default configuration variables
+config_variables = ["float_precision",
+                    "max_coor_value",
+                    "num_circle_min",
+                    "num_circle_min"]
 
 # Define functions
 
+def config_ask(default_message = True,
+               config_args = config_variables):
+    """Formats user command line input for configuration details"""
+    if default_message:
+        print("Enter configuration parameters for the following variables... ")
+        config_dictionary = dict()
+        for v in config_args:
+            config_dictionary.update({v:input("{}: ".format(v))})
 
+        return config_dictionary
+    else:
+        print(default_message)
+        config_dictionary = dict()
+        for v in config_args:
+            config_dictionary.update({v:input("{}: ".format(v))})
+
+        return config_dictionary
 # Define configuration helper class
 
 class helper:
@@ -16,10 +41,16 @@ class helper:
         # Check if configuration file exists, else create
         if os.path.isfile(self.config_path):
             # Read file as json
-            print("FIX")
+            with open(self.config_path, "r") as f:
+                # The config will is interpreted as a python dict
+                self.config = json.load(f)
+                f.close()
 
         else:
             os.makedirs(os.path.dirname(self.config_path), exist_ok = True)
             with open(self.config_path, "w") as f:
-                f.write("TEMP")
+                # Create formatted dictionary for configuration
+                self.config = config_ask()
+                # At this point, we only create the file
+                f.write(self.config)
                 f.close()
